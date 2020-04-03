b290f3da566e17061d022b21641a4c12ff9b6ca8
Wellington Silverio <wellington.alves.silverio@gmail.com>
2020-04-03 00:39:34 -0300

    Add points

--- src/containers/Game/index.js
+++ src/containers/Game/index.js
@@ -1,15 +1,17 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 export default function Game() {
+  const limitTime = 300;
+
+  let interval = null;
+
   const [display, setDisplay] = useState('');
   const [chances, setChances] = useState(0);
   const [points, setPoints] = useState(0);
-  const [firstNumber, setFirstNumber] = useState(0);
-  const [secondNumber, setSecondNumber] = useState(0);
-  const [sign, setSign] = useState(0); // 1 = +, 2 = -, 3 = *, 4 = /
   const [result, setResult] = useState(0);
   const [answer, setAnswer] = useState(0);
+  const [timer, setTimer] = useState(0);
 
   const initGame = async () => {
     selectEquation();
@@ -18,19 +20,51 @@ export default function Game() {
 
   const selectEquation = async () => {
     const numbers = [];
+
+    const maxNumber = Math.round(`${Math.floor(points)}`.length / 4);
+
     if (points < 1000) {
-      numbers[0] = Math.round(Math.random() * 10);
-      numbers[1] = Math.round(Math.random() * 10);
       numbers[2] = 1;
+    } else if (points < 10000) {
+      numbers[2] = Math.round(Math.random() * 1) + 1;
+    } else if (points < 1000000) {
+      numbers[2] = Math.round(Math.random() * 2) + 1;
     } else {
-      numbers[0] = Math.round(Math.random() * 10);
-      numbers[1] = Math.round(Math.random() * 10);
       numbers[2] = Math.round(Math.random() * 3) + 1;
     }
 
-    setFirstNumber(numbers[0]);
-    setSecondNumber(numbers[1]);
-    setSign(numbers[2]);
+    const decimal =
+      maxNumber - (numbers[2] - 1) > 4 ? 4 : maxNumber - (numbers[2] - 1);
+
+    numbers[0] = Math.round(Math.random() * 10 ** decimal);
+    numbers[1] = Math.round(Math.random() * 10 ** decimal);
+
+    if (numbers[0] === 0 && numbers[1] === 0) {
+      selectEquation();
+      return;
+    }
+
+    if (numbers[2] === 2) {
+      if (numbers[0] < numbers[1]) {
+        const tmp = numbers[0];
+        numbers[0] = numbers[1];
+        numbers[1] = tmp;
+      }
+    }
+
+    if (numbers[2] === 4) {
+      if (numbers[0] === 0 || numbers[1] === 0) {
+        selectEquation();
+        return;
+      }
+      if (numbers[0] < numbers[1]) {
+        const tmp = numbers[0];
+        numbers[0] = numbers[1];
+        numbers[1] = tmp;
+      }
+      numbers[0] = numbers[0] * numbers[1];
+    }
+
     setResult(
       numbers[2] === 1
         ? numbers[0] + numbers[1]
@@ -65,10 +99,13 @@ export default function Game() {
       setAnswer(response);
     } else {
       if (response === result) {
-        setPoints(points + 1);
+        setPoints(points => {
+          return points + (((limitTime - timer) / 100) * (points + 1)) / 2;
+        });
       } else {
-        setChances(chances - 1);
+        loseTurn();
       }
+      setTimer(0);
       skip();
     }
   };
@@ -77,6 +114,42 @@ export default function Game() {
     setAnswer(0);
     selectEquation();
   };
+
+  const loseTurn = () => {
+    if (chances <= 1) {
+      gameOver();
+      return;
+    }
+
+    setChances(chances => chances - 1);
+  };
+
+  const gameOver = () => {
+    setDisplay('');
+    setChances(0);
+    setAnswer(0);
+    setResult(0);
+  };
+
+  useEffect(() => {
+    if (chances > 0) {
+      if (timer >= limitTime) {
+        clearInterval(interval);
+        setTimer(0);
+        skip();
+        loseTurn();
+      } else {
+        interval = setInterval(() => {
+          setTimer(timer => timer + 1);
+        }, 100);
+      }
+    } else if (chances < 0 && timer !== 0) {
+      clearInterval(interval);
+      setTimer(0);
+    }
+
+    return () => clearInterval(interval);
+  }, [chances, timer]);
 
   return (
     <View style={[styles.col, {backgroundColor: '#F3FCF0'}]}>
@@ -106,13 +179,17 @@ export default function Game() {
       </View>
       <View
         style={[
+          styles.row,
           {
             flex: undefined,
             alignSelf: 'stretch',
+
             height: 8,
-            backgroundColor: 'red',
           },
-        ]}></View>
+        ]}>
+        <View style={{flex: limitTime - timer, backgroundColor: 'red'}}></View>
+        <View style={{flex: timer}}></View>
+      </View>
       <View style={[styles.col, {flex: 4, maxHeight: 380}]}>
         <View style={[styles.row, {marginTop: 8}]}>
           <View style={[styles.col, styles.button]}>
@@ -216,7 +293,16 @@ export default function Game() {
       </View>
       <View style={styles.col}>
         <Text style={[styles.text, {fontSize: 24}]}>
-          {answer > 0 ? answer : ''}
+          {answer > 0 ? `Resposta: ${answer}` : ''}
+        </Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={[styles.text, {fontSize: 24}]}>
+          {points > 0 ? Math.floor(points) : ''}
+        </Text>
+        <Text
+          style={[styles.text, {fontSize: 12, marginTop: 12, marginLeft: 4}]}>
+          {points > 0 ? `Pontos` : ''}
         </Text>
       </View>
     </View>
